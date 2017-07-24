f164a9478d58ac19ac257c3bc9b0eaf6cad75e30
Matthias Brettschneider <frosch03@frosch03.de>
2017-07-24 20:34:15 +0200

    adding movement functions

--- src/Chess.hs
+++ src/Chess.hs
@@ -204,3 +204,41 @@ data Move
     = Move Direction (Maybe Limit)
     deriving (Eq, Show)
 
+-- | Provide 'movesOf' with a 'Piece' and it will return the list of
+-- possible 'Move's the piece is able to make.
+movesOf :: Piece -> [Move]
+movesOf (Piece _     R) = [Move d Nothing  | d <- [0, 90, 180, 270]]
+movesOf (Piece _     N) = [Move d (Just 2) | d <- [27, 63, 117, 153, 207, 243, 297, 333]]
+movesOf (Piece _     B) = [Move d Nothing  | d <- [45, 135, 225, 315]]
+movesOf (Piece _     Q) = [Move d Nothing  | d <- [0, 45, 90, 135, 180, 225, 270, 315]]
+movesOf (Piece _     K) = [Move d (Just 1) | d <- [0, 45, 90, 135, 180, 225, 270, 315]]
+movesOf (Piece Black P) = [Move 270 (Just 1)]
+movesOf (Piece White P) = [Move  90 (Just 1)]
+
+-- | 'attacksOf' is the pendant to 'movesOf'. Within chess the attacks
+-- are the same as the moves for all figures but the pawn. For a pawn
+-- the attack fields are 1 away along the two diagonales facing the
+-- moving direction. This case is catched by the lower two guards.
+attacksOf :: Piece -> [Move]
+attacksOf p@(Piece c f)
+    | f /= P
+    = movesOf p
+
+    | f == P && c == White
+    = [Move d (Just 1) | d <- [45, 135]]
+
+    | f == P && c == Black
+    = [Move d (Just 1) | d <- [225, 315]]
+
+-- | The function 'deltaByMove' calculates the deltas for each
+-- coordinate and move. A delta summed together with a position will
+-- give you one possible move the piece is able to make. 
+deltaByMove :: Move -> (Int, Int)
+deltaByMove (Move d (Just l)) = (calc cos d, calc sin d)
+    where fromDeg d = d * pi / 180
+          calc trig = round . ((fromIntegral l) *) . trig . fromDeg . fromIntegral
+deltaByMove (Move d Nothing) = (calc cos d, calc sin d)
+    where fromDeg d = d * pi / 180
+          calc :: (Floating a, RealFrac a) => (a -> a) -> (Direction -> Int)
+          calc trig = round . trig . fromDeg . fromIntegral
+
